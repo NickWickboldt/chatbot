b9447a9a61f70bdb674c9975d5006f53d0e84d7c
Zuckuu <108366295+Zuckuu@users.noreply.github.com>
2024-03-28 18:07:19 -0500

    slide out animation, text generation page

--- stable-diffusion.js
+++ stable-diffusion.js
@@ -3,7 +3,7 @@ import {contentFilterText} from "./content-filter.js";
 const submitButton = document.querySelector(".submit-btn"); 
 const imageFrame = document.querySelector(".image-frame"); 
 const entry = document.querySelector(".image-gen-entry"); 
-const mainContainer = document.querySelector(".main-container")
+const displayDiv = document.querySelector('.display-frame');
 
 async function query(data) {
 	const response = await fetch(
@@ -53,12 +53,13 @@ async function submitClicked(){
 			entry.placeholder = "There has been an error."; 
 		}
 	}
+	display_input(input);
 }
 
 function display_input(input){
-	const displayDiv = document.createElement('div');
-
-	displayDiv.textContent = input;
-
-	document.mainContainer.appendChild(displayDiv);
+	displayDiv.classList.remove('slide-in');
+	displayDiv.classList.add('slide-out');
+	const inputText = document.createElement('h1');
+	inputText.textContent = "You typed: " + input;
+	displayDiv.appendChild(inputText);
 }
