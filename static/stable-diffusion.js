df62468457e860c73cdd2b562027be6c6671426c
Zuckuu <108366295+Zuckuu@users.noreply.github.com>
2024-03-29 14:41:49 -0500

    flask integrated

--- static/stable-diffusion.js
+++ static/stable-diffusion.js
@@ -1,27 +1,10 @@
 import {contentFilterText} from "./content-filter.js";
 
-const app = express();
-const PORT = 5500;
-
 const submitButton = document.querySelector(".submit-btn"); 
 const imageFrame = document.querySelector(".image-frame"); 
 const entry = document.querySelector(".image-gen-entry"); 
 const displayDiv = document.querySelector('.display-frame');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'bad'); // Specify the destination folder
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname); // Use the original filename
-    }
-});
-
-const upload = multer({ storage: storage });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 async function query(data) {
 	const response = await fetch(
@@ -53,13 +36,12 @@ async function submitClicked(){
 	if(contentValue == 1){
 		const img = document.createElement('img'); 
 		img.classList.add('image-frame-loading'); 
-		img.src = "../asset/image-loading.gif"; 
+		img.src = "../../static/asset/image-loading.gif"; 
 		imageFrame.appendChild(img); 
 		query({"inputs": input}).then((response) => {
 			img.classList.remove('image-frame-loading'); 
 			img.classList.add('image-frame-image'); 
 			img.src = URL.createObjectURL(response); 
-			upload.single(img.src);
 		});
 		entry.value = "";
 		entry.placeholder = "Type something in..."
