0b752b30687636ea62f7e18a14d3986c3ea1d21c
NickWickboldt <nickwickboldt@gmail.com>
2024-04-22 22:36:05 -0500

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def index():
